8a974808e58774c8afdb14be4168a9f00c316a96
kendell harris <kendell.harris@hgs.hiddengeniusproject.org>
2018-09-14 19:22:55 -0700

    commit successful

---
+++ bash.sh
@@ -0,0 +1,20 @@
+pwd			#shows you the file path 
+cd ~/desktop		#changes the directory
+mkdir website_filessytems			#creates folders
+cd website_filessytems			#changes the folder to website filesystem
+touch index.html		#makes file index.html
+mkdir html css js img extra 		#makes tfolders html.etc
+cd html				#going into a different folder
+touch pg1.html 		#creates file called pg1 html
+touch pg2.html      #creates file called pg2.html
+cd ../css			#takes you to the parent folder to the css folder
+touch pg1.css		#changes the file called pg1.css
+touch pg2.css		#creates the file called pg2.css
+touch index.css		#creates the folder called index.css
+cd ../js 			#going into the parent folder then to the js folder
+touch pg1.js 		#creates a file called pg1.js
+touch pg2.js		#creates a file called pg2.js
+touch index.js		#cretaes a file called index.js
+cd ../extra			#going to parent folder then takes you to extra folder
+mkdir audio			#makes a folder called audio
+mkdir video			#makes a folder called video 
